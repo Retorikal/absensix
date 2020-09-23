e0ff1b6705f90c3dfb474e73efca0f6cab5b3860
Wolfhysics <farreldzaudan2204@gmail.com>
2020-09-23 23:26:11 +0700

    remove debug line

--- popup/popup_utils.js
+++ popup/popup_utils.js
@@ -5,7 +5,6 @@ function updateConfig() {
     configVal[1] = document.getElementById("end_offset").value;
     configVal[2] = document.getElementById("repeat_delay").value;
     //Sending message to content
-    document.getElementById("updateButton").innerText = "clickedddd";
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, configVal);
     });
